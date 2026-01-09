26f518916a2997275969079dbc730bbb2b65f9b7
bryanhouiller-debug <bryan.houiller@le-campus-numerique.fr>
2026-01-09 15:42:33 +0100

    exercices 1 jour 3

---
+++ exercices/jour-03/foreach-simple.php
@@ -0,0 +1,13 @@
+<?php
+$fistNames = ["Pierre", "Marie", "Jean", "Sophie", "Lucas"];
+
+$i = 1;
+
+echo "<ul>";
+
+foreach ($fistNames as $fistNames) {
+     echo "<li>$i. $firstNames</li>";
+     $i++;
+}
+
+echo"</ul>";
