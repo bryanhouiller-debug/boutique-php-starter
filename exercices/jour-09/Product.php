8d3b50913906d543a8cd5f872170c7a41d962319
Bryan Houiller <bryan.houiller@le-campus-numerique.fr>
2026-01-19 11:28:51 +0100

    exercices 2

---
+++ exercices/jour-09/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+class Product
+{
+    public function __construct(
+        private string $name,
+        private float $price
+    ) {}
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
